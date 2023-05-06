821dff397d6454866073d7593c79aaa58b949e1e
Oorpow <309754513@qq.com>
2023-05-06 11:35:34 +0800

    feat: 初始化模板

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router'
+
+import App from './App.vue'
+import './samples/node-api'
+import './index.css'
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(router)
+app.use(pinia)
+
+app.mount('#app')
+  .$nextTick(() => {
+    postMessage({ payload: 'removeLoading' }, '*')
+  })
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="text-yellow-500">
+    home
+  </div>
+</template>
+
+<style scoped></style>
